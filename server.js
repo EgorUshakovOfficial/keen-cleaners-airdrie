0a26053bf14262a7595b403180f397f5c96e246f
EgorUshakovOfficial <egorushakov13@gmail.com>
2022-04-08 11:28:40 -0600

    make it better

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const nodeMailer = require('nodemailer');
+const path = require('path'); 
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://keen-cleaners.herokuapp.com/",
     credentials: true
 }));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -57,6 +58,15 @@ app.post('/contact', (req, res) => {
     })
     
 })
+
+if (process.env.NODE_ENV === "production") {
+    // Set static folder 
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html")); 
+    })
+}
 
 // Listen to server 
 const port = process.env.PORT || 4000;
