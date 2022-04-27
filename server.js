d8c4fe621c7ed70c8cd1661a9932c63920cb90b6
EgorUshakovOfficial <egorushakov13@gmail.com>
2022-04-27 10:17:28 -0600

    Server side rendering implemented

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+// Babel 
+require('ignore-styles');
+
+require('@babel/register')({
+    ignore: [/(node_modules)/],
+    presets: ['@babel/preset-env', '@babel/preset-react']
+})
+
 // Dotenv
 require('dotenv').config();
 
