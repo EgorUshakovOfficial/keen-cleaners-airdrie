597abed30e4077313a2ec9d0c8b9817a174a2210
EgorUshakovOfficial <egorushakov13@gmail.com>
2022-04-27 13:23:13 -0600

    Server side rendering implemented

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,7 +6,7 @@ import { useState, useEffect } from 'react';
 
 const Navbar = () => {
     // State
-    const [navMobState, setNavMobState] = useState("close")
+    const [navMobState, setNavMobState] = React.useState("close")
 
     // Handle navigate 
     const handleNavigate = () => {
