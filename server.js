e2a025f73daaa77b34a154a5dd8815253995071d
EgorUshakovOfficial <egorushakov13@gmail.com>
2022-04-27 10:21:31 -0600

    Server side rendering implemented

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static('client/build'));
 
     app.get('/', (req, res) => {
-        const app = ReactDOMServer.renderToString(<App />)
+        const app = ReactDOMServer.renderToString(App)
         const indexFile = path.resolve('./client/build/index.html')
 
         fs.readFile(indexFile, "utf-8", (err, data) => {
