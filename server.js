9e3f34a52b64f810136ae5761cc3a5828e23260f
EgorUshakovOfficial <egorushakov13@gmail.com>
2022-04-27 15:31:15 -0600

    commit

--- server.js
+++ server.js
@@ -68,7 +68,6 @@ app.post('/contact', (req, res) => {
 if (process.env.NODE_ENV === "production") {
     // Set static folder 
     app.use(express.static('client/build'));
-    app.use("*", express.static('client/build'));
 
     app.get("*", (req, res) => {
         const app = ReactDOMServer.renderToString(
