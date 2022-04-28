04fc12e68ecc1c6d631302b22d5790872802279a
EgorUshakovOfficial <egorushakov13@gmail.com>
2022-04-28 17:38:22 -0600

    Push final changes

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 require('ignore-styles');
 
 require('@babel/register')({
-    ignore: [/(node_modules)/]
+    ignore: [/(node_modules)/],
+    presets: ['@babel/preset-env', '@babel/preset-react', 'es2015']
 })
 
 
