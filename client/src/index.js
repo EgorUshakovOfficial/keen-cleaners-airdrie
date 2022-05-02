047c23caa790d5a88b11100eacfeb41bc8e745fe
EgorUshakovOfficial <egorushakov13@gmail.com>
2022-05-02 12:35:12 -0600

    Push final changes

--- client/src/index.js
+++ client/src/index.js
@@ -13,3 +13,6 @@ ReactDOM.render(
     </Router>,
 container);
 
+//Keen Cleaner's is a local business based off Airdrie, Alberta, that provides residential house cleaning services. I had the privilege to work with this company and create a user friendly website for its users to go on. I utilized React along with React Router to create the front-end of the website, and used Node and express to implement the back end. If you want to view this project live, please click the following link: https://keen-cleaners.herokuapp.com/
+
+
